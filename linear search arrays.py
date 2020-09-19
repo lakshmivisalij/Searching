96d4c983a04ebcacd2074e8407af6f4813cb7d70
Adilakshmi Visali Jayanthi <lakshmivisalij@gmail.com>
2020-09-19 19:49:14 +0530

    Update linear search arrays.py

--- linear search arrays.py
+++ linear search arrays.py
@@ -1,3 +1,6 @@
+
+# Linear Search Arrays Iteration
+
 t = int(input("Enter number of test cases: "))
 while t > 0:
 	n = int(input("Enter number of elements in an array: "))
@@ -13,3 +16,20 @@ while t > 0:
 	if f==0:
 	    print(-1)
 	t-=1
+	
+	
+# Linear Search Arrays Recursion
+
+def linearSearchRec(arr, key, i):
+    if len(arr)==0:
+        return -1
+    
+    if arr[0]==key:
+        return i
+    return linearSearchRec(arr[1:], key, i+1)
+    
+    return -1
+
+arr = [1, 2, 3, 4, 5, 6, 7]    
+index = linearSearchRec(arr, 5, 0)
+print(index)
